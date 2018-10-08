a60e3bdda2631d7f6585be51c156534c3202faff
raulares <raulares21@gmail.com>
2018-10-08 14:04:58 -0600

    Empezando

---
+++ prueba.cpp
@@ -0,0 +1,10 @@
+#include "clases.hpp"
+
+int main(){
+	Punto punto1;
+	punto1.AsignarX(5);
+	punto1.AsignarY(10);
+	cout << punto1.ObtenerX() << endl;
+	cout << punto1.ObtenerY() << endl;
+	return 0;
+}
